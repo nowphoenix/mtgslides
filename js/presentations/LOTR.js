7c3e9e8ee76f41f809d4637f1b6a1f061dddfe75
nowphoenix <91765849+nowphoenix@users.noreply.github.com>
2025-06-09 21:41:49 -0500

    added LOTR list

---
+++ js/presentations/LOTR.js
@@ -0,0 +1,171 @@
+var lotr = `//A Long Expected Party
+The Shire|Second Breakfast|Brandywine Farmer|Took Reaper|sandsteppe citadel@ltc|swords to plowshares@LTC|seasons past#529
+the party tree|Wizard's Rockets|Gandalf, Friend of the Shire#50|chromatic lantern@LTC|Bilbo, Birthday Celebrant#131|Delighted Halfling#158|Banquet Guests|Feasting Hobbit#37
+Bilbo, retired burglar|Birthday Escape|Rogue's Passage@ltc
+//PANORAMA
+//The Shadow of the Past
+bilbo's ring|Inherited Envelope|Gandalf, Friend of the Shire#800|The Ring Tempts You#H13|Fiery Inscription
+//Three Is Company
+Lobelia Sackville-Baggins|bag end|Green Dragon Inn
+Chance-met elves|elven chorus|essence warden@ltc
+//A Shortcut To Mushrooms
+brushland@ltc|Shortcut to Mushrooms|frodo, sauron’s bane#796|Mushroom Watchdogs|cultivate@LTC|Eastfarthing Farmer|bucklebury ferry
+//A Conspiracy Unmasked
+harmonize@LTC|the bath song|Meriadoc Brandybuck#177|Peregrin Took#181|Samwise Gamgee#222
+//The Old Forest
+murmuring bosk@LTC|timber protector@ltc|Old Man Willow|Tom Bombadil#823
+//In the House Of Tom Bombadil
+Tom Bombadil#234|second harvest@ltc|Goldberry, River-Daughter|soul’s attendant@ltc
+//Fog On the Barrow-Downs
+barrow-downs|Fog on the Barrow-Downs|crypt incursion@ltc|Bombadil's Song|barrow-blade
+//At the Sign of the Prancing Pony
+Inn of the Prancing Pony|Butterbur, Bree Innkeeper|Slip on the Ring
+//Strider
+Strider, Ranger Of the North
+//A Knife In The Dark
+ringwraiths|Fear, Fire Foes!
+Bill Ferny, Bree Swindler|Bill the Pony|barrow-blade
+fall of gil-galad|tale of tinuviel
+scoured barrens@ltc|Weathertop|living death@ltc|Strider, Ranger Of the North|Ranger's firebrand|night’s whisper@ltc|Ringsight
+morgul-knife|Morgul-knife Wound|sanguine bond@ltc|scattered groves@ltc
+//Flight To The Ford
+Glorfindel, Dauntless rescuer
+Horses of the Bruinen|devastation tide@ltc|Nazgul
+//BOOK TWO
+//Many Meetings
+Rejuvenating springs@ltc|Rivendell|Elrond, Master of healing|Elrond, lord of rivendell|Glóin, Dwarf Emissary|Arwen Undomiel
+//The Council Of Elrond
+council's deliberation|elrond of the white council|erestor of the council|plea for power@LTC
+Sol ring@ltc|sauron, lord of the rings|war of the last alliance|isildur’s fateful strike|relic of sauron|lost to legend|claim the precious|Gollum, patient plotter#309|Gollum, patient plotter|there and back again
+thought vessel@LTC|learn from the past@LTC|scroll of isildur|three rings for the elven-kings|Boon of the Wish-Giver@ltc|one ring to rule them all|in the darkness bind them
+the torment of gollum|Radagast, wizard of wilds|radagast the brown
+isengard, saruman's fortress|deceive the messenger|consider@ltc|saruman the white|Saruman's trickery|Saruman of Many colors|Isolation at orthanc|treason of isengard
+Gwaihir, greatest of the eagles|Gwaihir the windlord|escape from orthanc|Display of power
+//The Ring Goes South
+shards of narsil|forge anew|anduril, flame of the west|glamdring|gimli's axe|sting, the glinting dagger|mithril coat
+The Ring goes south|tempt with discovery@LTC|dunland crebain|revenge of ravens@LTC|Boromir, warden of the tower|Gimli of the glittering caves|Legolas, master archer|snarling warg
+redhorn pass|Legolas greenleaf|travel through caradhras
+//A Journey In The Dark
+deep analysis@LTC|monstrosity of the lake|Doors of Durin|mines of moria|gandalf the grey|stern scolding|gimli, mournful avenger|balin's tomb|book of mazarbul
+//The Bridge Of Khazad-Dum
+hobbit's sting|rush the room|swarming of moria|moria marauder|battle-scarred goblin|Troll of Khazad-dûm
+Bridge of Khazad-dûm|The Balrog, Flame of Udûn|The Balrog of Moria|The Balrog, Durin's Bane#195|You Cannot Pass!|Flame of Anor#203|pact of negation@ltc|Lash of the Balrog#92|Gandalf the Grey#322|gimli, mournful avenger#815
+//PANORAMA
+//Lothlorien
+Mirrormere Guardian|woodland stream@LTC|Lothlórien Lookout|Haldir, Lórien Lieutenant|galadhrim ambush|galadhrim guide|Lórien Revealed|tranquil thicket@ltc
+vineglimmer snarl@ltc|paradise druid@LTC|galadriel, elven-queen|celeborn the wise
+//The Mirror Of Galadriel
+galadriel of lothlorien|galadriel of lothlorien#813|dreadful as the storm|elvish harbinger@LTC|mirror of galadriel|Samwise Gamgee#819
+//Farewell to Lorien
+lembas|whispersilk cloak@LTC|lorien brooch|Galadriel, gift-giver|pristine talisman@LTC|elessar, the elfstone|galadhrim bow|gift of strands|phial of galadriel
+//The Great River
+aragorn, company leader|swan song@ltc|Sylvan tutor@ltc|gollum, obsessed stalker|argonath, pillars of the kings
+//The Breaking of the Fellowship
+visions of glory@LTC|Boromir, warden of the tower#794|breaking of the fellowship|lidless gaze|Sam, loyal attendant|aragorn, company leader#808 
+//PART II The Two Towers 
+//BOOK THREE
+//The Departure of Boromir
+horn of gondor|bitter downfall|Boromir, Gondor’s Hope
+//The Riders of Rohan 
+pristine talisman@ltc|riders of Rohan|eomer, marshal of rohan
+//The Uruk-hai
+surrounded by orcs|orcish medicine|merciless executioner@ltc|Ugluk of the White hand|uruk-hai berserker|Grishnákh, Brash Instigator|orcish Bowmasters|Mauhúr, Uruk-hai Captain
+//Treebeard
+Ent-Draught Basin|long list of the ents|treebeard, gracious host|treebeard, eldest of ents
+assemble the entmoot|quickbeam, upstart ent|blasphemous act@ltc|hew the entwood|fangorn forest|last March of the ents|last march of the ents#739
+//The White Rider
+Gandalf the White#305|frostboil snarl@ltc|Gandalf the White#299|Shadowfax, Lord of Horses
+//The King Of The Golden Hall
+Clifftop retreat@ltc|Edoras, Capital of Rohan|Meduseld, Golden Hall of Edoras|bewitching leechcraft|fact or fiction@ltc|Grima Wormtongue|faithless looting@ltc
+court of ire@ltc|banish from edoras|path to exile@ltc|quarrel’s end
+Theoden, Strength Restored|Theoden, king of rohan|rohirrim lancer|Herugrim, Sword of Rohan|Eowyn, Lady of Rohan
+//Helm’s Deep
+feed the swarm@ltc|westfold rider|Helm's Deep|orcish siegemaster|combat celebrant@ltc|fire of orthanc|thrill of possibility@ltc
+|gimli’s fury|legolas, counter of kills|gimli, counter of kills|friendly rivalry@ltr|glittering caves of aglarond|gimli of the glittering caves
+relentless rohirrim|taunt from the rampart|rally at the hornburg|Erkenbrand, Lord of Westfold|rising of the day
+//PANORAMA
+//The Road To Isengard
+isengard, saruman’s fortress|Merry, warden of Isengard|Pippin, warden of Isengard
+//Flotsam and Jetsam 
+enraged huorn|fangorn, tree shepherd|great oak guardian@ltc|woodfall primus@ltc|orchard strider@ltc|lignify@ltc|storm of saruman|fell the mighty@ltc|sunken hollow@ltc
+//PANORAMA Isengard destroyed
+//The Voice of Saruman
+Gandalf’s sanction|grima, saruman's footman
+//The Palantir
+Peregrin took#807|diabolic intent@ltc|Palantir of orthanc|commander’s sphere@ltc|voracious fell beast
+//BOOK FOUR
+//The Taming Of Smeagol
+Hithlain rope|hithlain knots|Smeagol, Helpful Guide
+//The Passage Of The Marshes
+smoldering marsh@ltc|the dead marshes|haunt of the dead marshes|underground river@ltc
+//The Black Gate Is Closed
+smeagol, helpful guide#822|the black gate|mordor muster
+//Of Herbs And Stewed Rabbit
+of Herbs and stewed rabbit|mortify@ltc|stew the coneys|rangers of ithilien|faramir, field commander 
+//The Window On The West
+henneth annun|faramir, field commander#795
+//The Forbidden Pool
+ithilien kingfisher|river kelpie@LTC|rangers of ithilien|soothing of smeagol|faramir, prince of ithilien#811
+//Journey To The Cross-Roads
+mind stone@ltc
+//The Stairs Of Cirith Ungol
+tower of cirith ungol|minas morgul|witch-king of angmar|choked estuary@ltc
+//PANORAMA
+//Shelob's Lair
+Gollum, scheming guide|shelob, child of ungoliant|frodo, determined hero|shelob's ambush|shelob, dread weaver
+//The Choices Of Master Samwise
+shelob, whose lair is death|Samwise the stouthearted|Cirith Ungol patrol|shagrat, loot bearer|gorbag of minas morgul|barad-dur
+//PART III The Return Of The King
+//BOOK FIVE
+//Minas Tirith
+Call for aid|explore@ltc|minas Tirith#256|minas Tirith#752|denethor, ruling steward|pippin, guard of the citadel|beregond of the guard|knights of dol amroth|shadow of the enemy
+//The Passing Of The Grey Company
+Dunedain rangers|merry, esquire of rohan|aragorn, the uniter#809|Press the enemy|Oath of the grey host|door of destinies@ltc|paths of the dead|stone of erech|shadow summoning@ltr 
+//The Muster Of Rohan
+Battlefield forge@ltc|errand-rider of gondor|riders of rohan|merry, esquire of rohan#817
+//The Siege Of Gondor
+//PANORAMA (Gandalf of the secret fire)
+Bastion protector@ltc|gandalf, white rider|pippin, guard of the citadel|prince imrahil the fair|stalwarts of osgiliath|protector of gondor
+siege-gang commander@ltc|mordor trebuchet|warbeast of gorgoroth|grond, the gatebreaker|rise of the witch-king
+//The Ride Of The Rohirrim
+weathered wayfarer@ltc|wose pathfinder|relentless rohirrim|forth eorlingas!|riders of the mark
+//The Battle Of The Pelennor Fields
+Lord of the nazgul|Decree of pain@ltc|Witch-king, bringer of ruin|Eowyn, fearless knight|abyssal persecutor@ltc|eowyn, shieldmaiden|witch-king of angmar#803|smite the deathless@ltr
+Eomer of the riddermark|Oliphaunt|easterling vanguard|gothmog, morgul lieutenant|haradrim spearmaster|borne upon a wind|vanquisher’s banner@ltc|banishing light@ltc
+//PANORAMA (battle in minas tirith)
+//The Pyre Of Denethor
+Dearly departed@ltc|Denethor, stone seer|pippin,  guard of the citadel#818
+//The Houses Of Healing
+Ioreth of the healing house|languish@ltc|worship@ltc
+//The Last Debate
+port town@ltc|corsairs of umbar|hostage taker@ltc|captain of umbar|grey host reinforcements|soldier of the grey host|king of the oathbreakers
+//The Black Gate Opens
+aragorn, the uniter|the black gate|the mouth of Sauron|sauron’s ransom|pippin’s bravery 
+//BOOK SIX
+//The Tower Of Cirith Ungol
+tower of Cirith Ungol|sam's desperate rescue|treasure nabber
+//The Land Of Shadow
+orcish Bowmasters 
+//Mount Doom
+valley of gorgoroth|mount Doom#258|call of the ring|Samwise the stouthearted#798
+fires of mount doom|frodo, sauron’s bane|frodo baggins#812|gollum’s bite|cast into the fire|the one ring|anguished unmaking@ltc|Sauron, the dark lord#821|crumbling necropolis@ltc
+//PANORAMA(bundle)
+//The Fields Of Cormallen 
+mount doom#754|sulfurous springs@ltc|gwaihir greatest of the eagles|landroval, horizon witness|meneldor, swift savior|Gandalf the white|Aragorn, king of gondor
+//The Steward And The King
+eowyn, lady of rohan|faramir, Steward of gondor|reprieve
+crown of gondor|Aragorn, King Of gondor|flowering of the white tree|Aragorn and Arwen, wed
+//Many Partings
+Necroblossom snarl@ltc|dawn of hope@ltc|eomer, king of Rohan|talisman of conviction@ltc|talisman of progress@ltc|horn of the mark
+many Partings|entish restoration|Saruman of many colors#820
+//Homeward Bound
+inn of the prancing pony|butterbur, Bree innkeeper|bill the pony
+//The Scouring Of The Shire
+lotho, corrupt shirriff|subjugate the hobbits|lobelia, defender of bag end|Sharkey, tyrant of the shire|spiteful banditry@ltr|notion thief@ltc|toxic deluge@ltc
+fortified village@ltc|shire shirriff|isolated chapel@ltc|field of ruin@ltc|call for unity@ltc|access tunnel@ltc|took reaper|the Battle of bywater|nasty end
+//PANORAMA(Rosie cotton)
+//The Grey Havens
+revive the shire|rampant growth@ltc|Samwise gamgee|Rosie cotton of south lane|Elanor Gardner 
+cirdan the shipwright|Gandalf, westward voyager|grey havens navigator|the grey havens|hinterland harbor@ltc
+//PANORAMA (many Partings)
+`;
